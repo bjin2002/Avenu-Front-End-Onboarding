dc2cd4302fd5365718ed9278a14ae3b897a094a1
benjin <benjin@umich.edu>
2021-09-03 16:33:24 -0400

    Initial Commit

---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import Todo from './Todo'
+
+export default function TodoList({ todos , toggleTodo}) {
+    return (
+        todos.map(todo => {
+            return <Todo key={todo.id} toggleTodo = {toggleTodo} todo={todo}/>
+        })
+    )
+}
